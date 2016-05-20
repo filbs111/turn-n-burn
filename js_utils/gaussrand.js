1489bbb3c8bed56015b37b12d61c72098a3c9111
filbs111 <ed.filby@gmail.com>
2016-05-20 23:03:03 +0100

    #11 precalculate random numbers for gaussian shot spray
    array of 256 numbers to cycle through.
    uses "immediately invoked function expression"
    
    http://benalman.com/news/2010/11/immediately-invoked-function-expression/

--- js_utils/gaussrand.js
+++ js_utils/gaussrand.js
@@ -1,13 +1,20 @@
-function gaussRand(){
-	//note currently generally becomes bigger as increase numTimes. (maybe should divide by sqrt(numtimes?))
+var gaussRand = (function(){
 	var sqrtNumTimes = 2;
 	var numTimes = sqrtNumTimes*sqrtNumTimes;
-	var total = -numTimes/2;
-	for (var ii=0;ii<numTimes;ii++){
-		total+=Math.random();
-	}
-	return total/sqrtNumTimes;
-}
+	var randoms = new Array();
+	var nextIdx =0;
+	for (var jj=0;jj<256;jj++){
+		var total = -numTimes/2;
+		for (var ii=0;ii<numTimes;ii++){
+			total+=Math.random();
+		}
+		randoms.push(total/sqrtNumTimes);
+	}	
+	return function(){
+			nextIdx = (nextIdx+1)&0xff;
+			return randoms[nextIdx];
+		};
+}());
 
 //this is inefficent to run every time.
 //TODO precalc some number (eg 256) of random numbers. keep in array. cycle through array, returning a random number each time.
