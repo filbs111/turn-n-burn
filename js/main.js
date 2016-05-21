16ac72e12c1c49a599bb300be1c8d75f3d7f098a
filbs111 <ed.filby@gmail.com>
2016-05-21 01:31:53 +0100

    move hi res timer to separate js file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-//high res timer thing. from http://stackoverflow.com/questions/6233927/microsecond-timing-in-javascript
-if (window.performance.now) {
-    myconsolelog("Using high performance timer");
-    getTimestamp = function() { return window.performance.now(); };
-} else {
-    if (window.performance.webkitNow) {
-        myconsolelog("Using webkit high performance timer");
-        getTimestamp = function() { return window.performance.webkitNow(); };
-    } else {
-        myconsolelog("Using low performance timer");
-        getTimestamp = function() { return new Date().getTime(); };
-    }
-}
-
 var gunCountdown;
 
 //hard code these for now. 
